8ff315f8cf3b2974910b28388624083690b5b238
xingshen <shenxing0707@gmail.com>
2026-09-18 14:18:59 +0800

    Filter audio cues by audible salience

--- backend/compiler.py
+++ backend/compiler.py
@@ -6,7 +6,7 @@ import os
 import re
 import time
 
-COMPILER_REVISION='singlecall.v37.compact_evidence'
+COMPILER_REVISION='singlecall.v38.audible_salience'
 REQUIRED_H3_SECTIONS = (
     'subject_definitions', 'summary', 'retention_analysis',
     'detailed_description', 'overall_soundscape', 'non_diegetic_music',
