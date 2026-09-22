2f62939a039af46da1cd58a0ef4c01c74e9eb485
xingshen <shenxing0707@gmail.com>
2026-09-23 01:14:20 +0800

    v56: concentrate execution detail in shot descriptions

--- backend/compiler.py
+++ backend/compiler.py
@@ -6,7 +6,7 @@ import os
 import re
 import time
 
-COMPILER_REVISION='singlecall.v55.concise_shot_planning'
+COMPILER_REVISION='singlecall.v56.detailed_execution_compact_anchors'
 REQUIRED_H3_SECTIONS = (
     'subject_definitions', 'summary', 'retention_analysis',
     'detailed_description', 'overall_soundscape', 'non_diegetic_music',
