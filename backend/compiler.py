a6cbe3683ec8f20e77538959661672778bcfb177
xingshen <shenxing0707@gmail.com>
2026-09-23 18:06:44 +0800

    Label v60 sound continuity compiler

--- backend/compiler.py
+++ backend/compiler.py
@@ -6,7 +6,7 @@ import os
 import re
 import time
 
-COMPILER_REVISION='singlecall.v59.concrete_transition'
+COMPILER_REVISION='singlecall.v60.sound_continuity'
 REQUIRED_H3_SECTIONS = (
     'subject_definitions', 'summary', 'retention_analysis',
     'detailed_description', 'overall_soundscape', 'non_diegetic_music',
