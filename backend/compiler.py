88b4d04d25f7173ec69c58bed6fffc8fb601332f
xingshen <shenxing0707@gmail.com>
2026-09-23 20:16:05 +0800

    Label v62 duration-bound outline compiler

--- backend/compiler.py
+++ backend/compiler.py
@@ -6,7 +6,7 @@ import os
 import re
 import time
 
-COMPILER_REVISION='singlecall.v61.necessary_cuts'
+COMPILER_REVISION='singlecall.v62.duration_bound_outline'
 REQUIRED_H3_SECTIONS = (
     'subject_definitions', 'summary', 'retention_analysis',
     'detailed_description', 'overall_soundscape', 'non_diegetic_music',
