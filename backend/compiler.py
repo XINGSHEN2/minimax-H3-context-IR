533272565d852bd1addb1720768d2710939f51d7
xingshen <shenxing0707@gmail.com>
2026-09-24 11:31:19 +0800

    Label v63 logical developments compiler

--- backend/compiler.py
+++ backend/compiler.py
@@ -6,7 +6,7 @@ import os
 import re
 import time
 
-COMPILER_REVISION='singlecall.v62.1.state_continuity'
+COMPILER_REVISION='singlecall.v63.logical_developments'
 REQUIRED_H3_SECTIONS = (
     'subject_definitions', 'summary', 'retention_analysis',
     'detailed_description', 'overall_soundscape', 'non_diegetic_music',
